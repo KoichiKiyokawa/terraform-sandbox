8ac0795459fffa56732581d3b1daa7c331d8cb1b
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-12-30 00:24:30 +0900

    :tada: init dynamic-rendering

---
+++ dynamic-rendering/front/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  console.log(window.location)
+</script>
+
+<svelte:head>
+  <title>{new Date().toISOString()}</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
