69a16573eecb157ca06c3e25fcb66bb9c11f5004
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-12-31 13:05:08 +0900

    chore: comment out nat_gateway and vpn_gateway

--- aws-module/vpc.tf
+++ aws-module/vpc.tf
@@ -4,12 +4,12 @@ module "vpc" {
   name = "aws-module-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-northeast-1a", "ap-northeast-1b"]
+  azs             = ["ap-northeast-1a", "ap-northeast-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
 
   tags = {
     Terraform   = "true"
